0965805f8d2cee6c8b617bf6afdae72e46e39af2
Maxim Dzhuliy <mak7.dj@gmail.com>
2016-03-20 12:36:06 +0200

    Update design and all files for it

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   def index
     @books = Book.all
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,23 @@
 class Book < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :slug_candidates, use: [:slugged, :finders]
+
   validates :title, :url, presence: true, uniqueness: true
+  validates :slug, uniqueness: true
+
+  private
+    def uid
+      SecureRandom.hex 2
+    end
+
+    def slug_candidates
+      [
+        :title,
+        [:uid, :title]
+      ]
+    end
+
+    def should_generate_new_friendly_id?
+      title_changed? || slug.blank?
+    end
 end
