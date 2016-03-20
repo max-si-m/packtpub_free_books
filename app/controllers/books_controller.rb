8b57ad7f3a8e8cd90d6cfac0a1106ba03a7c94ce
Maxim Dzhuliy <mak7.dj@gmail.com>
2016-03-20 16:28:20 +0200

    fix bug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.search(params[:q]).page(params[:page]).order_by(id: :desc)
+    @books = Book.search(params[:q]).page(params[:page]).order(id: :desc)
   end
 
   def show
